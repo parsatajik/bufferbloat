f82f763907c82c003db4a2b34b1fb0b09b7d6a65
Parsa Tajik <parsa1379.tajik@gmail.com>
2022-12-03 15:35:31 -0500

    added repo files

---
+++ README.md
@@ -0,0 +1,35 @@
+CSC458 PA2
+---
+
+This assignment was written by Parsa Tajik in December of 2022.
+
+Setup Instructions
+---
+
+
+Questions
+---
+
+__1. Why do you see a difference in webpage fetch times with small and large router buffers?__
+
+    answer goes here lolol
+
+
+__2. Bufferbloat can occur in other places such as your network interface card (NIC). Check the output of ifconfig eth0 on your VirtualBox VM. What is the (maximum) transmit queue length on the network interface reported by ifconfig? For this queue size and a draining rate of 100 Mbps, what is the maximum time a packet might wait in the queue before it leaves the NIC?__
+
+    answer goes here
+
+__3.How does the RTT reported by ping vary with the queue size? Write a symbolic equation to describe the relation between the two (ignore computation overheads in ping that might affect the final result).__
+
+    answer goes here
+
+__4.Identify and describe two ways to mitigate the bufferbloat problem.__
+
+    answer goes here
+
+
+
+
+
+
+
---
+++ bufferbloat.py
@@ -0,0 +1,270 @@
+#!/usr/bin/python
+"CSC458 Fall 2022 Programming Assignment 2: Bufferbloat"
+
+from mininet.topo import Topo
+from mininet.node import CPULimitedHost
+from mininet.link import TCLink
+from mininet.net import Mininet
+from mininet.log import lg, info
+from mininet.util import dumpNodeConnections
+from mininet.cli import CLI
+from mininet.clean import cleanup
+
+from subprocess import Popen, PIPE
+from time import sleep, time
+from multiprocessing import Process
+from argparse import ArgumentParser
+
+from monitor import monitor_qlen
+import termcolor as T
+
+import sys
+import os
+import math
+
+import helper
+
+# TODO: Don't just read the TODO sections in this code.  Remember that
+# one of the goals of this assignment is for you to learn how to use
+# Mininet.
+
+parser = ArgumentParser(description="Bufferbloat tests")
+parser.add_argument('--bw-host', '-B',
+                    type=float,
+                    help="Bandwidth of host links (Mb/s)",
+                    default=1000)
+
+parser.add_argument('--bw-net', '-b',
+                    type=float,
+                    help="Bandwidth of bottleneck (network) link (Mb/s)",
+                    required=True)
+
+parser.add_argument('--delay',
+                    type=float,
+                    help="Link propagation delay (ms)",
+                    required=True)
+
+parser.add_argument('--dir', '-d',
+                    help="Directory to store outputs",
+                    required=True)
+
+parser.add_argument('--time', '-t',
+                    help="Duration (sec) to run the experiment",
+                    type=int,
+                    default=10)
+
+parser.add_argument('--maxq',
+                    type=int,
+                    help="Max buffer size of network interface in packets",
+                    default=100)
+
+# Linux uses CUBIC-TCP by default that doesn't have the usual sawtooth
+# behaviour.  For those who are curious, invoke this script with
+# --cong cubic and see what happens...
+# sysctl -a | grep cong should list some interesting parameters.
+parser.add_argument('--cong',
+                    help="Congestion control algorithm to use",
+                    default="reno")
+
+# Expt parameters
+args = parser.parse_args()
+
+class BBTopo(Topo):
+    "Simple topology for bufferbloat experiment."
+
+    def build(self, n=2):
+        # Here are two hosts
+        hosts = []
+        for i in range(1,n+1):
+            hosts.append(self.addHost('h%d'%(i)))
+
+        # Here I have created a switch.  If you change its name, its
+        # interface names will change from s0-eth1 to newname-eth1.
+        switch = self.addSwitch('s0')
+
+        # TODO: Add links with appropriate characteristics
+        # Hosts
+        h1 = hosts[0]
+        h2 = hosts[1]
+
+        # Define required params for generating our links
+        bwHost = args.bw_host
+        bwNet = args.bw_net
+        delay = args.delay
+        maxq = args.maxq
+
+        # Establish link between home computer (h1) and router
+        self.addLink(h1, switch, bw=bwHost, delay='%sms'%delay, max_queue_size=maxq)
+
+        # Establish link between router and h2 (bottleneck)
+        self.addLink(switch, h2, bw=bwNet, delay='%sms'%delay, max_queue_size=maxq)
+
+
+
+# Simple wrappers around monitoring utilities.  You are welcome to
+# contribute neatly written (using classes) monitoring scripts for
+# Mininet!
+
+# tcp_probe is a kernel module which records cwnd over time. In linux >= 4.16
+# it has been replaced by the tcp:tcp_probe kernel tracepoint.
+def start_tcpprobe(outfile="cwnd.txt"):
+    os.system("rmmod tcp_probe; modprobe tcp_probe full=1;")
+    Popen("cat /proc/net/tcpprobe > %s/%s" % (args.dir, outfile),
+          shell=True)
+
+def stop_tcpprobe():
+    Popen("killall -9 cat", shell=True).wait()
+
+def start_qmon(iface, interval_sec=0.1, outfile="q.txt"):
+    monitor = Process(target=monitor_qlen,
+                      args=(iface, interval_sec, outfile))
+    monitor.start()
+    return monitor
+
+def start_iperf(net):
+    h2 = net.get('h2')
+    print "Starting iperf server..."
+    # For those who are curious about the -w 16m parameter, it ensures
+    # that the TCP flow is not receiver window limited.  If it is,
+    # there is a chance that the router buffer may not get filled up.
+    server = h2.popen("iperf -s -w 16m")
+
+    # TODO: Start the iperf client on h1.  Ensure that you create a
+    # long lived TCP flow. You may need to redirect iperf's stdout to avoid blocking.
+    h1 = net.get('h1')
+    time = args.time
+
+    print "Starting iperf client..."
+
+    h1.cmd("iperf -t %s -c %s" % (time, h2.IP()))
+
+def start_webserver(net):
+    h1 = net.get('h1')
+    proc = h1.popen("python http/webserver.py", shell=True)
+    sleep(1)
+    return [proc]
+
+def start_ping(net):
+    # TODO: Start a ping train from h1 to h2 (or h2 to h1, does it
+    # matter?)  Measure RTTs every 0.1 second.  Read the ping man page
+    # to see how to do this.
+
+    # Hint: Use host.popen(cmd, shell=True).  If you pass shell=True
+    # to popen, you can redirect cmd's output using shell syntax.
+    # i.e. ping ... > /path/to/ping.txt
+    # Note that if the command prints out a lot of text to stdout, it will block
+    # until stdout is read. You can avoid this by running popen.communicate() or
+    # redirecting stdout
+    
+    # hosts
+    h1 = net.get('h1')
+    h2 = net.get('h2')
+
+    # TODO: if graphs as not looking correct, try chaging -c to -w
+    popen = h1.popen(
+        "ping -c %s -i 0.1 %s > %s/ping.txt"%
+        (args.time*10, h2.IP(), args.dir), 
+        shell=True)
+
+    # block until stdout is read
+    popen.communicate()
+
+def get_avg_time(net, h1, h2):
+    times = []
+    for i in range(3):
+        # construct fetch command
+        # TODO: check if this is correct
+        cmd = "curl -o /dev/null -s -w %{time_total} %s/http/index.html" % (h1.IP())
+        # get current time
+        curr_time = h2.popen(cmd).communicate()[0]
+        times.append(curr_time)
+        
+    # return the avg of our three fetches
+    return helper.avg(times)
+
+def bufferbloat():
+    if not os.path.exists(args.dir):
+        os.makedirs(args.dir)
+    os.system("sysctl -w net.ipv4.tcp_congestion_control=%s" % args.cong)
+
+    # Cleanup any leftovers from previous mininet runs
+    cleanup()
+
+    topo = BBTopo()
+    net = Mininet(topo=topo, host=CPULimitedHost, link=TCLink)
+    net.start()
+    # This dumps the topology and how nodes are interconnected through
+    # links.
+    dumpNodeConnections(net.hosts)
+    # This performs a basic all pairs ping test.
+    net.pingAll()
+
+    # Start all the monitoring processes
+    start_tcpprobe("cwnd.txt")
+    start_ping(net)
+
+    # TODO: Start monitoring the queue sizes.  Since the switch I
+    # created is "s0", I monitor one of the interfaces.  Which
+    # interface?  The interface numbering starts with 1 and increases.
+    # Depending on the order you add links to your network, this
+    # number may be 1 or 2.  Ensure you use the correct number.
+    #
+    qmon = start_qmon(iface='s0-eth2',
+                      outfile='%s/q.txt' % (args.dir))
+
+    # TODO: Start iperf, webservers, etc.
+    start_iperf(net)
+    start_webserver(net)
+
+    # Hint: The command below invokes a CLI which you can use to
+    # debug.  It allows you to run arbitrary commands inside your
+    # emulated hosts h1 and h2.
+    #
+    # CLI(net)
+
+    # TODO: measure the time it takes to complete webpage transfer
+    # from h1 to h2 (say) 3 times.  Hint: check what the following
+    # command does: curl -o /dev/null -s -w %{time_total} google.com
+    # Now use the curl command to fetch webpage from the webserver you
+    # spawned on host h1 (not from google!)
+    # Hint: have a separate function to do this and you may find the
+    # loop below useful.
+    
+    # hosts
+    h1 = net.get("h1")
+    h2 = net.get("h2")
+    
+    # params
+    stats = []
+    start_time = time()
+    while True:
+        # do the measurement (say) 3 times.
+        curr_stat = get_avg_time(net, h1, h2)
+        stats.append(curr_stat)
+        
+        sleep(1)
+        now = time()
+        delta = now - start_time
+        if delta > args.time:
+            break
+        print "%.1fs left..." % (args.time - delta)
+
+    # TODO: compute average (and standard deviation) of the fetch
+    # times.  You don't need to plot them.  Just note it in your
+    # README and explain.
+    print "Storing stats..."
+    f = open("./stats.txt", "w+")
+    f.write("Latency Average: %s \n" % helper.avg(stats))
+    f.write("Latency Standard Deviation: %s \n" % helper.stdev(stats))
+    f.close()
+
+    stop_tcpprobe()
+    if qmon is not None:
+        qmon.terminate()
+    net.stop()
+    # Ensure that all processes you create within Mininet are killed.
+    # Sometimes they require manual killing.
+    Popen("pgrep -f webserver.py | xargs kill -9", shell=True).wait()
+
+if __name__ == "__main__":
+    bufferbloat()
